b77b68b87a514bb3e3a53e6081fcbc7528d1cbf3
vkosuri <malli.kv2@gmail.com>
2020-04-05 13:28:45 +0530

    download tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,13 +1,32 @@
+
 import json
-import httplib2
-import io
-import logging
 import mock
-import os
 import unittest2 as unittest
-import random
-import socket
-import ssl
-import time
+from file_service import FileService
 
 class TestDownload(unittest.TestCase):
+    def test_file_download(self):
+        pass
+
+    def test_raise_site_conversion(self):
+        fs = FileService()
+        file_list = {'files': [{'id': '0', 'name': 'Test', 'mimeType': 'mimeType:application/vnd.google-apps.site', 'starred': False, 'trashed': False, 'owners': [{'kind': 'drive#user', 'displayName': 'Malliarjunarao Kosuri', 'me': True, 'permissionId': '123', 'emailAddress': 'rhelvkosuri@gmail.com'}]}]}
+        assert fs.download(file_list['files']) == False
+
+    def test_raise_map_conversion(self):
+        fs = FileService()
+        file_list = {'files': [{'id': '0', 'name': 'Test', 'mimeType': 'mimeType:application/vnd.google-apps.map', 'starred': False, 'trashed': False, 'owners': [{'kind': 'drive#user', 'displayName': 'Malliarjunarao Kosuri', 'me': True, 'permissionId': '123', 'emailAddress': 'rhelvkosuri@gmail.com'}]}]}
+        assert fs.download(file_list['files']) == False
+
+    def test_raise_drawing_conversion(self):
+        fs = FileService()
+        file_list = {'files': [{'id': '0', 'name': 'Test', 'mimeType': 'mimeType:application/vnd.google-apps.drawing', 'starred': False, 'trashed': False, 'owners': [{'kind': 'drive#user', 'displayName': 'Malliarjunarao Kosuri', 'me': True, 'permissionId': '123', 'emailAddress': 'rhelvkosuri@gmail.com'}]}]}
+        assert fs.download(file_list['files']) == False
+
+    def test_raise_form_conversion(self):
+        fs = FileService()
+        file_list = {'files': [{'id': '0', 'name': 'Test', 'mimeType': 'mimeType:application/vnd.google-apps.form', 'starred': False, 'trashed': False, 'owners': [{'kind': 'drive#user', 'displayName': 'Malliarjunarao Kosuri', 'me': True, 'permissionId': '123', 'emailAddress': 'rhelvkosuri@gmail.com'}]}]}
+        assert fs.download(file_list['files']) == False
+
+if __name__ == '__main__':
+    unittest.main()
