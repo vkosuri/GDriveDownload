e6f298369c849d5bb30ff45a28958486ea14ca0a
vkosuri <malli.kv2@gmail.com>
2020-04-05 09:34:58 +0530

    search files by args

--- examples/download.py
+++ examples/download.py
@@ -17,32 +17,33 @@ import os
 # the argument parser, see the logging_cfg method
 logger = None
 
+
+parser = argparse.ArgumentParser(description='Process input arguments.')
+parser.add_argument('--name', required=True, type=str, help='please input file name to download')
+parser.add_argument('--logfile', default="drivelog.log", required=False, type=str, help='logger file')
+args = parser.parse_args()
+
+# Logging
+logging_cfg(args.logfile)
+
+# get values from environment
+access_token = os.environ.get('ACCESS_TOKEN')
+refresh_token = os.environ.get('REFRESH_TOKEN')
+token_uri = os.environ.get('TOKEN_URI')
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process input arguments.')
-    parser.add_argument('--name', required=True, type=str, help='please input file name to download')
-    parser.add_argument('--logfile', default="drivelog.log", required=False, type=str, help='logger file')
-    args = parser.parse_args()
-
-    # Logging
-    logging_cfg(args.logfile)
-
-    # get values from environment
-    access_token = os.environ.get('ACCESS_TOKEN')
-    refresh_token = os.environ.get('REFRESH_TOKEN')
-    token_uri = os.environ.get('TOKEN_URI')
-    client_id = os.environ.get('CLIENT_ID')
-    client_secret = os.environ.get('CLIENT_SECRET')
-
     # login to Google drive
     ds = DriveClient()
     ds.login(access_token, refresh_token, token_uri, client_id, client_secret)
     cred = ds.get_credentials()
 
-    # create file services
-    # https://developers.google.com/apps-script/reference/drive
+    # create file services https://developers.google.com/apps-script/reference/drive
     fs = FileService()
     fs.create_service(drive='drive',api_version='v3',credentials=cred)
-    files = fs.list_files('Test')
+
+    files = fs.list_files(args.name)
     for file in files:
         print("file_id:{0}, file_name:{1}, mimeType:{2}".format(file['id'], file['name'], file['mimeType']))
     # download files
